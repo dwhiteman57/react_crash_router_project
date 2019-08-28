1bb9f374af27d6650ca3e67daa402081172939e9
Daniel Whiteman <daniel.whiteman1@gmail.com>
2019-08-28 11:54:09 -0500

    Loading project files

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="main-content not-found">
+    <h2>Sorry, something went wrong. The page you are looking for does not exist</h2>
+  </div>
+);
+
+export default NotFound;
